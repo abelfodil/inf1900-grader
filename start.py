ca574bce926e38632940d10a6b80b1d696b89603
Anes Belfodil <ans.belfodil@gmail.com>
2018-12-24 15:44:15 -0500

    removed most of the logic from start.py

--- start.py
+++ start.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
 
-from os import path
 from cloning import clone
 from grading import grade
 from compiling import compile_grades
-from json import dump, load
-
-student_list_file = "students.json"
 
 print("Welcome to INF1900 interactive grading tool")
 
 grading_directory = None
 assignment_name = None
-student_list = None
 group = None
-
-if path.isfile(student_list_file):
-    with open('students.json', 'r') as f:
-        student_list = load(f)
 
 while True:
     choice = input("What do you want to do? Please choose one of the following: "
                    "clone, grade, compile ").strip()
 
     if choice == "clone":
-        grading_directory, group, student_list = clone()
-        with open(student_list_file, 'w') as f:
-            dump(student_list, f)
+        grading_directory, group = clone()
     elif choice == "grade":
         grading_directory, group, assignment_name = grade(grading_directory, group)
     elif choice == "compile":
-        compile_grades(grading_directory, assignment_name, student_list)
+        compile_grades(grading_directory, assignment_name)
     else:
         print("Incorrect parameter, please try again.")
