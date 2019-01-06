79ba36e17517a6ced6ebdb19b2161987e600f7d7
Anes Belfodil <anes.belfodil@polymtl.ca>
2019-01-06 16:12:49 -0500

    edited mail format

--- src/mail.py
+++ src/mail.py
@@ -4,15 +4,15 @@ import smtplib
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
 from os.path import isfile
 
 from src.ask import get_grading_directory
 from src.ask import get_grader_email
 
-default_subject = "[DO NOT REPLY] inf1900-grader"
-
-# You can spam that one
 default_receiver = "olivier-dion@hotmail.com"
+default_subject = "[NO-REPLY] inf1900-grader"
+default_message = "Correction d'un travail terminée."
 
 
 class MailException(Exception):
@@ -21,24 +21,22 @@ class MailException(Exception):
 
 
 class MailAttachment:
-    def __init__(self, type_, filename):
+    def __init__(self, type_, path, filename):
+        self.path = path
         self.filename = filename
         self.main_type, self.sub_type = type_.split("/")
 
     def to_MIME(self):
-        with open(self.filename, "rb") as f:
-            base = MIMEBase(self.main_type,
-                            self.sub_type)
+        with open(self.path, "rb") as f:
+            base = MIMEBase(self.main_type, self.sub_type)
             base.set_payload(f.read())
-            base.add_header("Content-Disposition",
-                            "attachment",
-                            filename=self.filename)
+            base.add_header("Content-Disposition", "attachment", filename=self.filename)
 
             return base
 
 
 class Mail:
-    def __init__(self, subject, sender, receiver, attachments=[]):
+    def __init__(self, sender, receiver, subject, message="", attachments=[]):
 
         msg = MIMEMultipart()
 
@@ -46,10 +44,9 @@ class Mail:
         msg["From"] = sender
         msg["To"] = receiver
 
-        self.filename_list = []
+        msg.attach(MIMEText(message))
 
         for attachment in attachments:
-            self.filename_list.append(attachment.filename)
             msg.attach(attachment.to_MIME())
 
         self.msg = msg
@@ -76,8 +73,8 @@ def mail():
         print(f"{filename} does not exist. Please assemble your grades first.")
         return
 
-    attachments = [MailAttachment("text/csv", filename)]
-    mail = Mail(default_subject, sender, receiver, attachments)
+    attachments = [MailAttachment("text/csv", filename, filename.replace("/", "_"))]
+    mail = Mail(sender, receiver, default_subject, default_message, attachments)
 
     print(f"""
 Send file: {filename}
