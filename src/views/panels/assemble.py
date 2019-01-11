748ec3d353c6d56a034eaf6495c9fe4d08c80de6
Anes Belfodil <ans.belfodil@gmail.com>
2019-01-11 15:23:12 -0500

    added fields to assemble

--- src/views/panels/assemble.py
+++ src/views/panels/assemble.py
@@ -4,12 +4,11 @@
 # Olivier Dion - 2019 #
 #######################
 
-from urwid import AttrMap, Columns, LineBox, ProgressBar
+from urwid import Columns, LineBox
 
-from src.models.mail import mail
+from src.models import assemble
 from src.models.state import state
 from src.views.base.buffer import Buffer
-from src.views.base.button import Button
 from src.views.base.form import Form
 from src.views.base.signal import Signal
 from src.views.panels.abstract import AbstractPanel
@@ -19,42 +18,13 @@ from src.views.panels.abstract import AbstractPanel
 class AssemblePanel(AbstractPanel):
 
     def __init__(self):
-        sender    = LineBox(Buffer(("header", "Sender email\n\n"), state.sender_email))
-        recipient = LineBox(Buffer(("header", "Recipient email\n\n"), state.recipient_email))
-        column1   = Columns([sender, recipient])
-
         grading_directory = LineBox(Buffer(("header", "Grading directory\n\n"), state.grading_directory))
-        subject  = LineBox(Buffer(("header", "Subject\n\n"), state.subject))
-        column2  = Columns([grading_directory, subject])
+        assignment_sname = LineBox(Buffer(("header", "Assignment short name\n\n"), str(state.assignment_sname)))
+        column = Columns([grading_directory, assignment_sname])
 
-        message  = LineBox(Buffer(("header" ,"Message\n\n"), state.message, multiline=True))
+        form = Form(assemble,
+                    grading_directory=grading_directory,
+                    assignment_sname=assignment_sname)
 
-        confirm  = LineBox(AttrMap(Button("Confirm",
-                                          on_press=self.confirm,
-                                          align="center"),
-                                   "default",
-                                   "confirm_button"))
-
-        abort    = LineBox(AttrMap(Button("Abort",
-                                          on_press=self.abort,
-                                          align="center"),
-                                   "default",
-                                   "abort_button"))
-
-        buttons_column  = Columns([confirm, abort])
-
-        bar      = ProgressBar("progress_low",
-                               "progress_hight",
-                               current=10)
-
-        form = Form(mail,
-                    sender_email=sender,
-                    recipient_email=recipient,
-                    subject=subject,
-                    message=message,
-                    grading_directory=grading_directory)
-
-        super().__init__(column1, form)
-        self.tree.split_vertically(column2)
-        self.tree.split_vertically(message)
-        self.tree.split_vertically(buttons_column)
+        super().__init__(column, form)
+        self.tree.split_vertically(self.buttons_column)
