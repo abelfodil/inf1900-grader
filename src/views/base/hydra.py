8795f58975fd6b80d14a12b4785d7c18457350c8
Anes Belfodil <ans.belfodil@gmail.com>
2019-01-12 15:32:30 -0500

    simplified hydra

--- src/views/base/hydra.py
+++ src/views/base/hydra.py
@@ -8,190 +8,47 @@ from urwid import Text
 
 
 class HydraHead:
+    def __init__(self, letter, letter_color, func, hint, params={}):
+        self.letter = letter
+        self.letter_color = letter_color
+        self.func = func
+        self.hint = hint
+        self.params = params
 
-    def __init__(self, hydra, letter, func, hint="", exit_=None, params={}):
-
-        self.hydra        = hydra
-        self.exit_        = hydra.exit_
-        self.func         = func
-        self.hint         = hint
-        self.letter       = letter
-        self.params       = params
-
-        if exit_ == Hydra.nil or exit_ == Hydra.t:
-            self.exit_ = exit_
-
-    def __str__(self):
-        if self.hint != "":
-            return "[{}]: {}".format(self.letter, self.hint)
-        else:
-            return self.letter
-
-    def __call__(self):
-
-        if self.func is not None:
-            self.func(**self.params)
-
-        if self.exit_ == Hydra.t:
-            try:
-                self.hydra.on_kill()
-            except AttributeError:
-                pass
-
-
-class Hydra:
-
-    # nil and true
-    nil      = False
-    t        = True
-
-    # Colors
-    red      = 1
-    blue     = 2
-    amaranth = 3
-    teal     = 4
-    pink     = 5
-
-    # Foreign-keys
-    warn     = 1
-    run      = 2
-
-    def __init__(self, name, heads, info="",
-                 color=None, foreign_keys=False, exit_=False,
-                 params={}):
-
-        self.foreign_keys = foreign_keys
-        self.exit_        = exit_
-        self.info         = info
-        self.heads        = {}
-        self.name         = name
-        self.params       = params
-
-        if color == Hydra.red:
-            pass
-
-        elif color == Hydra.blue:
-            self.exit_ = Hydra.t
-
-        elif color == Hydra.amaranth:
-            self.foreign_keys = Hydra.warn
-
-        elif color == Hydra.teal:
-            self.foreign_keys = Hydra.warn
-            self.exit_        = Hydra.t
-
-        elif color == Hydra.pink:
-            self.foreign_keys = Hydra.run
-
-        self.add_heads(heads)
-
-    def __str__(self):
-
-        heads_str = []
-
-        for head_letter in self.heads:
-            heads_str.append(str(self.heads[head_letter]))
-
-        heads_str = ", ".join(heads_str)
-
-        return "{}\n{}".format(self.info, heads_str)
-
+    def urwid_text(self):
+        return [("", "["), (self.letter_color, self.letter), ("", f"]: {self.hint} ")]
 
     def __call__(self):
         self.func(**self.params)
 
-    def add_heads(self, heads):
-
-        for head in heads:
-
-            letter = head[0]
-            func   = head[1]
-            hint   = ""
-            exit_  = None
-            params = {}
-
-            try:
-                hint = head[2]
-                if not isinstance(hint, str):
-                    hint = ""
-            except:
-                pass
-
-            try:
-                exit_ = head[3]
-
-                if exit_ != Hydra.nil and exit_ != Hydra.t:
-                    exit_ = None
-            except:
-                pass
-
-            try:
-                params = head[4]
-                if not isinstance(params, dict):
-                    params = {}
-            except:
-                pass
-
-            self.heads[letter] = HydraHead(self, letter, func, hint, exit_, params)
-
-
 
 class HydraWidget(Text):
-
-    def __init__(self, hydra=None, *kargs, **kwargs):
-
+    def __init__(self, info, *kargs, **kwargs):
         super().__init__("", *kargs, **kwargs)
 
         self.keybind = {}
-        self.hydra   = hydra
+        self.heads = {}
+        self.info = info
 
-        if hydra is not None:
-            self.parse_hydra()
-
-        # Glitch
+        # Glitch to make it work
         self._selectable = True
 
-    def set_hydra(self):
-        self.hydra = hydra
-        self.parse_hydra()
-
     def parse_hydra(self):
+        markup = [("", f"{self.info}\n")]
+        for letter, head in self.heads.items():
+            markup.extend(head.urwid_text())
 
-        markup  = []
-
-        # To refractor if better idea on how to make that
-
-        markup.append(("", f"{self.hydra.info}\n"))
-
-        for letter, head in self.hydra.heads.items():
-
-            if head.exit_ == Hydra.t:
-                tmp_letter = ("blue_head", head.letter)
-            else:
-                tmp_letter = ("red_head", head.letter)
-            if head.hint != "":
-                markup.append(("", "["))
-                markup.append(tmp_letter)
-                markup.append(("", f"]: {head.hint}"))
-            else:
-                markup.append(tmp_letter)
-
-            markup.append(("", ", "))
-
-        markup.pop()
-
-        self.set_text(markup if len(markup) != 0 else "")
+        self.set_text(markup)
 
     def keypress(self, size, key):
-
-        if key in self.hydra.heads:
-            self.hydra.heads[key]()
+        if key in self.heads:
+            self.heads[key]()
             return None
 
         return key
 
-
     def add_heads(self, heads):
-        if self.hydra is not None:
-            self.hydra.add_heads(heads)
-            self.parse_hydra()
+        for head in heads:
+            self.heads[head[0]] = HydraHead(*head)
+
+        self.parse_hydra()
--- src/views/panels/main.py
+++ src/views/panels/main.py
@@ -7,7 +7,7 @@
 from urwid import Filler
 
 from src.views.base.buffer import Controller, Signal
-from src.views.base.hydra import Hydra, HydraWidget
+from src.views.base.hydra import HydraWidget
 from src.views.base.tui import TUI
 from src.views.panels.assemble import AssemblePanel
 from src.views.panels.clone import ClonePanel
@@ -19,11 +19,7 @@ from src.views.panels.mail import MailPanel
 class MainPanel(HydraWidget, Controller):
 
     def __init__(self):
-        hydra = Hydra("MainView", [],
-                      info="Welcome to INF1900 interactive grading tool!",
-                      color=Hydra.blue)
-
-        super().__init__(hydra=hydra, align="center")
+        super().__init__(info="Welcome to INF1900 interactive grading tool!", align="center")
 
         self.add_views([
             ("c", ClonePanel(), "Clone"),
@@ -33,33 +29,21 @@ class MainPanel(HydraWidget, Controller):
         ])
 
         self.add_actions([
-            ("q", TUI.quit, "Quit"),
+            ("q", "red_head", TUI.quit, "Quit"),
         ])
 
         self.root = Filler(self, valign="bottom")
 
     def add_views(self, views):
-
         heads = []
-
         for letter, view, hint, in views:
             view.connect("on_quit", self.restore)
-            heads.append((letter, self.swap_view, hint, None, {"view": view,
-                                                               "hint": hint}))
+            heads.append((letter, "blue_head", self.swap_view, hint, {"view": view, "hint": hint}))
 
         self.add_heads(heads)
 
-    def add_actions(self, actions):
-
-        if not isinstance(actions, list):
-            actions = [actions]
-
-        heads = []
-
-        for action in actions:
-            heads.append((action[0], action[1], action[2]))
-
-        self.add_heads(heads)
+    def add_actions(self, actions: list):
+        self.add_heads(actions)
 
     def swap_view(self, view, hint):
         self.emit("on_swap", view, hint)
