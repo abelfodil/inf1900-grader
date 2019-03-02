1b6039a6358618ee086f7bd8c15434a39c8419e5
Anes Belfodil <ans.belfodil@gmail.com>
2019-03-02 15:37:19 -0500

    Create merge view

--- src/views/panels/merge.py
+++ src/views/panels/merge.py
@@ -1,11 +1,11 @@
 from urwid import Edit, LineBox
 
-from src.models.assemble import assemble
+from src.models.merge import merge
 from src.models.state import state
 from src.views.widgets.form import Form
 
 
-class AssemblePanel(Form):
+class MergePanel(Form):
 
     def __init__(self):
         grading_directory = LineBox(Edit(("header", "Grading directory\n\n"), state.grading_directory))
@@ -15,4 +15,4 @@ class AssemblePanel(Form):
             {"grading_directory": grading_directory, "assignment_sname": assignment_sname},
         ]
 
-        super().__init__("Assemble", grid_elements, assemble)
+        super().__init__("Merge", grid_elements, merge)
