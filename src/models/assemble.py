cf5b0def1e2d3e07646a40821a722453f0dcfda1
Anes Belfodil <ans.belfodil@gmail.com>
2021-02-06 20:36:25 -0500

    Reimplement CSV writer

--- src/models/assemble.py
+++ src/models/assemble.py
@@ -2,11 +2,11 @@ from csv import writer
 from datetime import datetime
 from statistics import mean, stdev
 import re
+from io import StringIO
 
 from src.models.clone import read_grading_info
 from src.models.grade import generate_grading_file_name, get_teams_list
-from src.models.validate import InvalidInput, ensure_grading_directory_exists, ensure_not_empty, \
-    time_format
+from src.models.validate import InvalidInput, ensure_grading_directory_exists, ensure_not_empty, time_format
 
 TOTAL_KEYWORD = "Total"
 GRADE_REGEX = fr"{TOTAL_KEYWORD}\D*(\d+(?:\.|,)\d+|\d+)\s*/"
@@ -27,30 +27,38 @@ def extract_grade(team: str, grading_directory: str, assignment_sname: str):
         raise InvalidInput(f"Missing or invalid grade for team {team}.")
 
 
+def add_grade_to_student_info(student_info: list, grades_map: dict):
+    return {**student_info,
+            'grade': grades_map[student_info["team"]]
+            }
+
+
 def write_grades_file(grading_directory: str, grades_map: dict, assignment_sname: str):
     info = read_grading_info(grading_directory)
     group_number = info["group_number"]
 
+    csv_output = StringIO()
+    csv_writer = writer(csv_output, delimiter=';')
+    csv_writer.writerows([
+        ["Cours:", "INF1900"],
+        ["Correcteur:", info["grader_name"]],
+        ["Section:", group_number],
+        ["Date:", datetime.now().strftime(time_format)],
+        ["Travail:", assignment_sname],
+        [],
+        ["Moyenne:", mean(grades_map.values())],
+        ["Écart-type:", stdev(grades_map.values())],
+        [],
+        ["Nom", "Prénom", "Équipe", "Note"],
+        *[list(add_grade_to_student_info(student_info, grades_map).values()) for student_info in info["students"]]
+    ])
+
+    # Hack: replace dot decimals with comma decimals
+    csv_output = csv_output.getvalue().replace(".", ",")
+
     grades_path = f"{grading_directory}/notes-inf1900-sect0{group_number}-{assignment_sname}.csv"
     with open(grades_path, 'w', newline='', encoding="utf-8") as csvfile:
-        csv_writer = writer(csvfile, delimiter=';')
-
-        csv_writer.writerow(["Cours:", "INF1900"])
-        csv_writer.writerow(["Correcteur:", info["grader_name"]])
-        csv_writer.writerow(["Section:", group_number])
-        csv_writer.writerow(["Date:", datetime.now().strftime(time_format)])
-        csv_writer.writerow(["Travail:", assignment_sname])
-
-        csv_writer.writerow([])
-        csv_writer.writerow(["Moyenne:", mean(grades_map.values())])
-        csv_writer.writerow(["Écart-type:", stdev(grades_map.values())])
-
-        csv_writer.writerow([])
-        csv_writer.writerow(["Nom", "Prénom", "Équipe", "Note"])
-
-        for student_info in info["students"]:
-            student_info["grade"] = str(grades_map[student_info["team"]]).replace(".", ",")
-            csv_writer.writerow(student_info.values())
+        csvfile.write(csv_output)
 
 
 def assemble(grading_directory: str, assignment_sname: str):
