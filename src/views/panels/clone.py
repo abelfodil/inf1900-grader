6d802ef39c8ae8e80d509335aafef4f3ab5461c0
Anes Belfodil <ans.belfodil@gmail.com>
2021-03-13 21:43:22 -0500

    Ask for grader's git username and password when cloning

--- src/views/panels/clone.py
+++ src/views/panels/clone.py
@@ -13,10 +13,13 @@ class ClonePanel(Form):
         grader_name = LineBox(Edit(("header", "Grader's name\n\n"), state.grader_name))
         group_number = LineBox(IntEdit(("header", "Group number\n\n"), state.group_number))
         team_type = RadioGroup("Team type", TeamType, state.team_type)
+        username = LineBox(Edit(("header", "Username\n\n"), ""))
+        password = LineBox(Edit(("header", "Password\n\n"), "", mask="*"))
 
         grid_elements = [
             {"grading_directory": grading_directory, "grader_name": grader_name},
             {"group_number": group_number, "team_type": team_type},
+            {"username": username, "password": password},
         ]
 
         super().__init__("Clone", grid_elements, clone)
